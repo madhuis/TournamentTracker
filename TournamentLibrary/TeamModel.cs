57eb61b932488734c476abf8fd1876fd15a6ade3
Ishan Madhu <imadhu2991@gmail.com>
2018-11-30 16:20:05 -0600

    Added two properties in TeamModelclass

---
+++ TournamentLibrary/TeamModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TournamentLibrary
+{
+    public class TeamModel
+    {
+        public List<Person> TeamMembers { get; set; } = new List<Person>();
+
+        // Equivalent to = new List<Person>();
+        //public TeamModel()
+        //{
+        //    TeamMembers = new List<Person>();
+        //}
+
+        public string TeamName { get; set; }
+    }
+}
