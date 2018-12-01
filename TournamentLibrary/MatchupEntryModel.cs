72343d6fc37356ac49e84f327dcf72ff6d34ef5f
Ishan Madhu <imadhu2991@gmail.com>
2018-12-01 10:12:47 -0600

    Library class has been added

---
+++ TournamentLibrary/MatchupEntryModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TournamentLibrary
+{
+    public class MatchupEntryModel
+    {
+        /// <summary>
+        /// Represents one team in the MatchUp
+        /// </summary>
+        public TeamModel TeamCompeting { get; set; }
+
+        /// <summary>
+        /// Represents scores from the particular team
+        /// </summary>
+        public double Score { get; set; }
+
+        /// <summary>
+        /// Represents the Matchup that this team came from the Winner
+        /// </summary>
+        public MatchupModel ParentMatchup { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="initialScore">
+        /// 
+        /// </param>
+        public MatchupEntryModel(double initialScore)
+        {
+
+        }
+    }
+}
---
+++ TournamentLibrary/MatchupModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TournamentLibrary
+{
+    public class MatchupModel
+    {
+        public List<MatchupEntryModel> Entries { get; set; } = new List<MatchupEntryModel>();
+
+        public TeamModel Winner { get; set; }
+
+        public int MatchupRound { get; set; }
+    }
+}
