f78e966b5b54fba19c541c91b4f6ce4dcb0633d9
Ishan Madhu <imadhu2991@gmail.com>
2018-12-05 23:35:54 -0600

    Comment has been added for CreatePrize Method

--- TournamentLibrary/SqlConnector.cs
+++ TournamentLibrary/SqlConnector.cs
@@ -8,6 +8,12 @@ namespace TournamentLibrary
 {
     public class SqlConnector : IDataConnection
     {
+        //TODO - Make the createPrize Method actually save the database
+        /// <summary>
+        /// Saves a new Prize to the database
+        /// </summary>
+        /// <param name="model">The Prize Information</param>
+        /// <returns>The Prize Info, Including the Unique Identifier.</returns>
         public PrizeModel CreatePrize(PrizeModel model)
         {
             model.Id = 1;
