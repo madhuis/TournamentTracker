6802402ed971dee222e8ffc90b8f7d8c581c9c7f
Ishan Madhu <imadhu2991@gmail.com>
2018-12-05 23:31:13 -0600

    Id Property has been added to the PrizeModel class.

--- TournamentLibrary/PersonModel.cs
+++ TournamentLibrary/PersonModel.cs
@@ -9,6 +9,8 @@ namespace TournamentLibrary
 {
     public class PersonModel
     {
+        public int Id { get; set; }
+
         public string FirstName { get; set; }
 
         public string LastName { get; set; }
