2ef5a8271de90369e5bf8595bebb94ade3a80ad2
Ishan Madhu <imadhu2991@gmail.com>
2018-12-05 23:30:18 -0600

    Another SqlConnector Class created , W/OUT Implementation of CreatePrize method

---
+++ TournamentLibrary/GlobalConfig.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TournamentLibrary
+{
+    public static class GlobalConfig
+    {
+        // We can save data connections and/or Text files that is why we used List here.
+        public static List<IDataConnection> Connections { get; private set; }
+
+        public static void InitializeConnections(bool database, bool textFiles)
+        {
+            if (database) // true value => database == true
+            {
+                // TODO = Create a Sql connection
+            }
+
+            if (textFiles)
+            {
+                // TODO = Create a Text Connection
+            }
+        }
+
+        
+    }
+}
---
+++ TournamentLibrary/SqlConnector.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TournamentLibrary
+{
+    public class SqlConnector : IDataConnection
+    {
+        public PrizeModel CreatePrize(PrizeModel model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
