2ca91f95bddebff009ac5186ec7e503bf2d4f117
Ishan Madhu <imadhu2991@gmail.com>
2018-12-05 22:22:47 -0600

    Interface for DataConnection has been created. It has only one method so far: CreatePrize Method

---
+++ TournamentLibrary/IDataConnection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TournamentLibrary
+{
+    public interface IDataConnection
+    {
+
+        PrizeModel CreatePrize(PrizeModel model); // We want just one method here which can create a Prizemodel.
+    }
+}
