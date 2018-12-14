2d1f9255e1f908669e2d4c0ce354b60eb8bf44e1
Ishan Madhu <imadhu2991@gmail.com>
2018-12-13 19:08:06 -0600

    Set up the Data layer and add all assembly for the Project

--- TournamentLibrary/DataAccess/IDataConnection.cs
+++ TournamentLibrary/DataAccess/IDataConnection.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TournamentLibrary.Models;
 
-namespace TournamentLibrary
+namespace TournamentLibrary.DataAccess
 {
     public interface IDataConnection
     {
--- TrackerUI/CreatePrize.cs
+++ TrackerUI/CreatePrize.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using TournamentLibrary;
+using TournamentLibrary.DataAccess;
+using TournamentLibrary.Models;
 
 namespace TrackerUI
 {
